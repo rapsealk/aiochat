3899db47330179d57e8d1ae40ea5c6d7d8b5f310
rapsealk <jskang@lablup.com>
2022-05-11 14:26:29 +0900

    feat: Add notification message of user in and out

--- src/aiochat/controllers/websocket.py
+++ src/aiochat/controllers/websocket.py
@@ -13,6 +13,22 @@ REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
 REDIS_CHANNEL_ID = 'aiochat-channel'
 
 
+class Message:
+    def __init__(self, tag: str, uuid: str, message: str):
+        self._tag = tag
+        self._uuid = uuid
+        self._message = message
+
+    def json(self) -> str:
+        return json.dumps({
+            'tag': self._tag,
+            'uuid': self._uuid,
+            'message': self._message,
+            'timestamp': time.time() * 1000,
+            'pid': os.getpid()
+        })
+
+
 class WebSocketController(BaseController):
     async def handle(self, request: web.Request):
         ws = web.WebSocketResponse()
@@ -29,22 +45,36 @@ class WebSocketController(BaseController):
                     await asyncio.sleep(0.01)
                 except asyncio.TimeoutError:
                     pass
+                except ConnectionResetError:
+                    break
+            await channel.unsubscribe(REDIS_CHANNEL_ID)
 
         future = asyncio.create_task(consume_message(redis.pubsub(), ws))
 
         uuid = str(uuid4())
 
+        await redis.publish(
+            REDIS_CHANNEL_ID,
+            Message(tag='event', uuid=uuid, message=f'{uuid} has joined.').json()
+        )
+
         async for message in ws:
+            if ws.closed:
+                break
             if message.data == 'close':
                 await ws.close()
                 break
-            await redis.publish(REDIS_CHANNEL_ID, json.dumps({
-                'uuid': uuid,
-                'message': json.loads(message.data).get('message', ''),
-                'timestamp': time.time() * 1000,
-                'pid': os.getpid()
-            }))
+            await redis.publish(
+                REDIS_CHANNEL_ID,
+                Message(tag='message', uuid=uuid, message=json.loads(message.data).get('message', '')).json()
+            )
 
         await future
+
+        await redis.publish(
+            REDIS_CHANNEL_ID,
+            Message(tag='event', uuid=uuid, message=f'{uuid} has left.').json()
+        )
+        await redis.close()
 
         return ws
--- static/js/index.js
+++ static/js/index.js
@@ -3,15 +3,30 @@ window.addEventListener('DOMContentLoaded', function(e) {
     const button = document.querySelector('#button');
     const chatBox = document.querySelector('#chatbox');
 
-    function generateChatBubbleItem({ uuid, message, timestamp, pid }) {
+    messageInput.focus();
+
+    function genMessageBubble({ uuid, message, timestamp, pid }) {
         const li = document.createElement('li');
         li.innerHTML = `[${pid}] <b>${uuid}</b>: ${message}<div style="display: inline; color: gray; font-size: 8pt; margin-left: 16px; text-align: right;">${new Date(timestamp).toLocaleString()}</div>`;
         return li;
     }
 
+    function genNotificationBubble({ message }) {
+        const li = document.createElement('li');
+        li.style.color = 'gray';
+        li.style.fontSize = '10pt';
+        li.innerHTML = message;
+        return li;
+    }
+
     const ws = new WebSocket(`ws://${window.location.host}/ws`);
     ws.onmessage = function(e) {
-        chatBox.appendChild(generateChatBubbleItem(JSON.parse(e.data)));
+        const data = JSON.parse(e.data);
+        if (data.tag === 'message') {
+            chatBox.appendChild(genMessageBubble(data));
+        } else if (data.tag === 'event') {
+            chatBox.appendChild(genNotificationBubble(data));
+        }
     };
 
     button.addEventListener('click', function(e) {
