674fd4548d7fc852960ef9b173a4a2794cbbd424
rapsealk <jskang@lablup.com>
2022-05-13 18:55:03 +0900

    fix: Remove unused `aiohttp-session`

--- src/aiochat/main.py
+++ src/aiochat/main.py
@@ -1,13 +1,9 @@
 import os
 
-# import aioredis
 import jinja2
 from aiohttp import web
 from aiohttp_jinja2 import setup as setup_aiohttp_jinja2
-# from aiohttp_session import setup as setup_aiohttp_session
-# from aiohttp_session.redis_storage import RedisStorage
 
-# from aiochat.common import REDIS_HOST
 from aiochat.controllers import ChatController, WebSocketController
 
 STATIC_FILE_PATH = os.path.abspath(
@@ -22,10 +18,6 @@ async def create_app() -> web.Application:
     app = web.Application()
 
     setup_aiohttp_jinja2(app, loader=jinja2.FileSystemLoader(JINJA_TEMPLATE_PATH))
-
-    # redis_pool = await aioredis.from_url(f'redis://{REDIS_HOST}', encoding='utf-8', decode_responses=True)
-    # storage = RedisStorage(redis_pool=redis_pool)
-    # setup_aiohttp_session(app, storage=storage)
 
     app.add_routes([
         web.get('/', ChatController()),
